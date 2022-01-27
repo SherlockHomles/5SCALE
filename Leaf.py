5bdd4cb4ea80412f8d6cb49ad670a7ae62da9c19
SherlockHolmes <blowman@smail.nju.edu.cn>
2022-01-27 20:28:51 +0800

    Merge branch 'main' of https://github.com/SherlockHomles/5SCALE
     Conflicts:
    	AOPDomain.py

--- Leaf.py
+++ Leaf.py
@@ -1,3 +1,9 @@
+import sys, os
+tmp_path = os.path.abspath(__file__)
+tmp_path = os.path.dirname(tmp_path)
+tmp_path = os.path.dirname(tmp_path)
+tmp_path = os.path.join(tmp_path, 'prosail')
+sys.path.append(tmp_path)
 from typing import Optional
 from prosail.prospect_d import run_prospect
 import numpy as np
